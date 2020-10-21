743a9a4741d4eb2292775dabbd2299a53b997124
John Sciutto <johnsciutto@gmail.com>
2020-10-20 21:15:38 -0400

    Servidor usando el modulo fs

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const http = require('http');
+const fs = require('fs');
+
+const port = 2000;
+
+const server = (req, res) => {
+  fs.readFile('front/index.html', (error, content) => {
+    if (error) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Salio mal');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    }
+  });
+};
+
+http.createServer(server).listen(port);
