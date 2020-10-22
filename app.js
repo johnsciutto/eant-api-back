6b2d4906c8933aca24becb6655ffb0deb291142a
John Sciutto <johnsciutto@gmail.com>
2020-10-22 19:17:47 -0400

    Cambiar servidor a Express

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const port = 2000;
+const puerto = 2000;
 
-const server = (req, res) => {
-  fs.readFile('front/index.html', (error, content) => {
-    if (error) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Salio mal');
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(content);
-    }
-  });
-};
+const app = express();
 
-http.createServer(server).listen(port);
+app.listen(puerto)
+
+app.get('/contacto' (req, res) => {
+
+});
