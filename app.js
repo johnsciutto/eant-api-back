ed9bbbf380b41eafd38f4da1a2f3d064b0da7fef
John Sciutto <johnsciutto@gmail.com>
2020-10-22 19:26:01 -0400

    Crear 'end' endpoint

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const puerto = 2000;
 
 const app = express();
 
-app.listen(puerto)
+app.listen(puerto);
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/contacto' (req, res) => {
+app.get('/contacto', (req, res) => {
+  res.end('Este es un formulario de contacto');
+});
 
+app.post('/end', (req, res) => {
+  console.log(req.body);
 });
