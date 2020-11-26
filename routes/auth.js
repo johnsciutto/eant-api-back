d1026a2e9967b5a16c2f463590b966350abfbaff
John Sciutto <johnsciutto@gmail.com>
2020-11-26 18:34:36 -0500

    feat(auth): Instalar cookie-parser

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const { JWT_SECRET } = process.env;
 
@@ -7,26 +8,25 @@ const auth = express();
 
 auth.use(express.urlencoded({ extended: true }));
 auth.use(express.json());
+auth.use(cookieParser());
 
-auth.route('/')
-  .get((req, res) => {
-    const jwtPayload = {
-      name: 'John Sciutto',
-      email: 'johnsciutto@gmail.com',
-      expiresIn: 1400,
-    };
+auth.get('/', (req, res) => {
+  const jwtPayload = {
+    name: 'John Sciutto',
+    email: 'test@test.com',
+    expiresIn: '3hs',
+  };
 
-    const token = jwt.sign(jwtPayload, JWT_SECRET);
+  const token = jwt.sign(jwtPayload, JWT_SECRET);
 
-    res.setHeader('Set-Cookie', [`token=${token}; HttpOnly; SameSite=secure; path=/`]);
+  res.setHeader('Set-Cookie', [`token=${token}; HttpOnly; SameSite=secure; path=/`]);
 
-    res.send('Check if the token is in the authorization header');
-  });
+  res.send('Check if the token is in the authorization header');
+});
 
-auth.route('/test')
-  .get((req, res) => {
-    const { cookie } = req.headers;
-    res.send(`the cookie was read, and it was: ${cookie}`);
-  });
+auth.get('/test', (req, res) => {
+  const { cookie } = req.headers;
+  res.send(`the cookie was read, and it was: ${cookie}`);
+});
 
 module.exports = auth;
