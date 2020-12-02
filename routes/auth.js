47a0a9debe96f5044676d12fb02547c19807d688
John Sciutto <johnsciutto@gmail.com>
2020-12-02 13:25:16 -0500

    fix(auth route): Eliminar rutas innecesarias

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-const { verifyToken, logInUser, signInUser } = require('../middleware/authorization');
+const { logInUser, signInUser } = require('../middleware/authorization');
 
 const auth = express();
 
@@ -53,30 +52,6 @@ auth.post('/login', async (req, res) => {
     res.cookie(userCookie);
     res.send(`This now redirects to the panel... The given cookie is: </br> ${userCookie}`);
   }
-});
-
-auth.get('/', (req, res) => {
-  res.send('This is the log-in page. To actually log in, click the button: </br> <form action="./" method="POST"><button type="submit">Log-In</button></form>');
-});
-
-auth.post('/', (req, res) => {
-  req.body.username = 'johnsciutto';
-  req.body.password = '12345';
-
-  const cookie = authenticateUser(req.body);
-  if (!cookie) res.send('Cookie not created because authentication failed');
-  else {
-    res.cookie(...cookie);
-    res.send('I\'ve created the token and sent it as a cookie. Click <a href="./test">Here</a> to see the actual contents of the cookie!');
-  }
-});
-
-auth.get('/test', verifyToken, (req, res) => {
-  res.send(`Here is the token: ${req.cookies._auth}`);
-});
-
-auth.get('/expired', (req, res) => {
-  res.send('The token expired, please log in again. <a href="./">Click here to generate another token</a>');
 });
 
 module.exports = auth;
